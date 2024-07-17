25687c064000d48e3467368d622e493389d19ac3
Zeynep Çakır <zeey3481.zc@gmail.com>
2024-07-17 13:40:55 +0300

    Add project files.

---
+++ ifElse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir  sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan büyüktür");
+
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı 10'dur.");
+            }
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir.");
+            }
+            else if (sayi == 1)
+            {
+                Console.WriteLine("Sayı tektir.");
+            }
+            else if (sayi == 0)
+            {
+                Console.WriteLine("Sayı negatiftir.");
+            }
+
+            Console.WriteLine("Programdan çıkmak için bir tuşa basınız.");
+            Console.ReadKey();
+
+        }
+    }
+}
+
